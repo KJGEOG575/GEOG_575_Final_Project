eb39d338ba90f86f4d40e5cb7364f427e994922b
KJGEOG575 <63277435+KJGEOG575@users.noreply.github.com>
2020-04-29 17:32:21 -0600

    backup version of csv

--- js/viz.js
+++ js/viz.js
@@ -1,28 +1,44 @@
 // wrap everything in self execting anon function to move the local scope
 (function() {
 
-	var attrArray = ["2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010"];
-    var costAttrArray = ["Structures", "Fatalities", "EconomicCo"];
+	//Array for reading values of state acreage by year in US_States_Data_Selection.csv
+    var attrArray = ["2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010"];
+    
+    //Array for reading the 3 cost related columns of the fire data.csv
+    var costAttrArray = ["Structures", "Fatalities", "Economic_Cost"];
 
+    var expressed = attrArray[0]; //initial attribute of the states acreage data
+    var costExpressed = costAttrArray[0]; //initial attribute of the fire damage cost data
 
-	var expressed = attrArray[0]; //initial attribute
-    var costExpressed = costAttrArray[2]; //initial attribute
-
-
-	//chart frame dimensions
-	var chartWidth = window.innerWidth * 0.25,
-		chartHeight = 500,
-		leftPadding = 35,
-		rightPadding = 2,
-		topBottomPadding = 5,
-		chartInnerWidth = chartWidth - leftPadding - rightPadding,
-		chartInnerHeight = chartHeight - topBottomPadding * 2,
-		translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+	//chart frame dimensions for the US states chart in Section 1
+	var chartWidth_S = window.innerWidth * 0.25,
+		chartHeight_S = 500,
+		leftPadding_S = 35,
+		rightPadding_S = 2,
+		topBottomPadding_S = 5,
+		chartInnerWidth_S = chartWidth_S - leftPadding_S - rightPadding_S,
+		chartInnerHeight_S = chartHeight_S - topBottomPadding_S * 2,
+		translate_S = "translate(" + leftPadding_S + "," + topBottomPadding_S + ")";
+    
+    //chart frame dimensions for the fire polygons relating to cost chart in Section 2
+	var chartWidth_F = window.innerWidth * 0.25,
+		chartHeight_F = 500,
+		leftPadding_F = 35,
+		rightPadding_F = 2,
+		topBottomPadding_F = 5,
+		chartInnerWidth_F = chartWidth_F - leftPadding_F - rightPadding_F,
+		chartInnerHeight_F = chartHeight_F - topBottomPadding_F * 2,
+		translate_F = "translate(" + leftPadding_F + "," + topBottomPadding_F + ")";
 
 	//create a scale to size bars proportionally to frame and for axis
-	var yScale = d3.scaleLinear()
+	var yScale_S = d3.scaleLinear()
 		.range([463, 0])
 		.domain([0, 3000]);
+    
+    //create a scale to size bars proportionally to frame and for axis
+	var yScale_F = d3.scaleLinear()
+		.range([463, 0])
+		.domain([0, 2000]);
 
 
 	var zoomSettings = {
@@ -70,12 +86,13 @@
         //use d3-queue to parallelize asynchronous data loading
 	d3.queue()
 		.defer(d3.csv, "data/US_States_Data_Selection.csv") //load attributes from csv
+        .defer(d3.csv, "data/FireData.csv")//load csv attributes for fire polygons
 		.defer(d3.json, "data/US_StatesTopojson.topojson") //load background spatial data
 		.defer(d3.json, "data/US_States_selection_topo.topojson") //load choropleth spatial data
-		.defer(d3.json, "data/Fire_Poly.topojson") //load fires polygon data
+		.defer(d3.json, "data/Fire_Polygon.topojson") //load fires polygon data
 		.await(callback); //trigger callback function once data is loaded
 
-		function callback(error, csvData, countryData, statesData, firesData) {
+		function callback(error, csvData, csvfireData, countryData, statesData, firesData) {
 
 			//place graticule on the map
 			setGraticule(g, path);
@@ -83,7 +100,7 @@
 			//translate usStates topojson
 			var naCountries = topojson.feature(countryData, countryData.objects.US_States), //load background spatial data
 				selectStates = topojson.feature(statesData, statesData.objects.US_States_selection).features, //load choropleth data
-				firePolygons = topojson.feature(firesData, firesData.objects.Fire_Poly_with_data).features;  //load fire data
+				firePolygons = topojson.feature(firesData, firesData.objects.Fire_Buffer_Clean).features;  //load fire data
 
 			console.log(selectStates);
 			console.log(firePolygons);
@@ -94,29 +111,39 @@
 				.attr("class", "countries")
 				.attr("d", path);
 
-			// join csv data to Geojson enumerration units
+			// join csv data to topojson enumerration units for the state polygons
 			selectStates = joinData(selectStates, csvData);
+            
+            // join csv data to topojson enumeration units fire poly units
+			firePolygons = joinDataFirePoly(firePolygons, csvfireData);
 
-			// create color scale
+			// create color scale states
 			var colorScale = makeColorScale(csvData);
+            
+            // create color scale forest fire cost polygons
+			var colorScaleCost = makeColorScaleCost(csvfireData);
 
 			//add enumeration units to the map
 			setEnumerationUnits(selectStates, g, path, colorScale);
 
-			//add coordinated viz to map
+			//add coordinated viz to map for states
 			setChart(csvData, colorScale);
+            
+            //add coordinated viz to map for fire polygons in the cost category
+			/*setChartFire(csvfireData, colorScale);*/
 
 			createDropdown(csvData);
+                    
+            toggleFires(path, g, firePolygons, colorScaleCost);
             
-            toggleFires(path, g, firePolygons);
+            createCostFireDropdown(csvfireData)
 
-            
-            createCostFireDropdown(firePolygons);
+           
 
 		};
 	};   // end of setMap()
 
-	//function to create color scale generator
+	//function to create color scale generator for states
 	function makeColorScale(data) {
 		var colorClasses = [
         "#FEF0D9",
@@ -151,6 +178,42 @@
 
 		return colorScale;
 	};
+    
+	//function to create color scale generator for fire polygons
+	function makeColorScaleCost(data) {
+		var colorClassesCost = [        
+        "#f6eff7",
+        "#bdc9e1",
+        "#67a9cf",
+        "#02818a"
+		];
+
+		// create color scale generator
+		var colorScaleCost = d3.scaleThreshold()
+			.range(colorClassesCost);
+
+		// build array of all values of the expressed attribute
+		var domainArrayCost = [];
+		for (var j=0; j < data.length; j++){
+			var costVal = parseFloat(data[j][costExpressed]);
+			domainArrayCost.push(costVal);
+		};
+
+		// cluster data using ckmeans clustering algorith to create natural breaks
+		var costClusters = ss.ckmeans(domainArrayCost, 5);
+		//reset domain array to cluster minimums
+		domainArrayCost = costClusters.map(function (d) {
+			return d3.min(d);
+		});
+
+		// remove first value from domain array to create class breakpoints
+		domainArrayCost.shift();
+
+		//assign array of last 4 cluster minimums as domain
+		colorScaleCost.domain(domainArrayCost);
+
+		return colorScaleCost;
+	};
 
 	function setGraticule(map, path) {
 		//create graticule generator
@@ -172,7 +235,8 @@
 			.attr("d", path); //project graticule lines
 
 	};
-
+    
+    //function to join states json and csv data
 	function joinData(selectStates, csvData){
 		for (var i = 0; i < csvData.length; i++) {
 			var csvState = csvData[i]; // the current state
@@ -208,7 +272,43 @@
 
 		return selectStates;
 	};
+        //function to join fire polys and fire csv data
+    	function joinDataFirePoly(firePolygons, csvfireData){
+		for (var j = 0; j < csvfireData.length; j++) {
+			var csvfirePoly = csvfireData[j]; // the current state
+			var csvfirePolyKey = csvfirePoly.fire_name; //the csv primary key
 
+			//loop through geojson states to find correct state
+			for (var b = 0; b < firePolygons.length; b++) {
+				var topojsonProps = firePolygons[b].properties; // the current state geojson properties
+
+				var topojsonKey = topojsonProps.fire_name //the geojson primary key
+
+				//where primary keys match, transfer csv data to geojson properties object
+				if (topojsonKey == csvfirePolyKey) {
+
+					// assign all attributes and values
+					costAttrArray.forEach((function (attr) {
+						var valCost = parseFloat(csvfirePoly[attr]);  // get csv attribute value
+
+						console.log("var valCost = " + valCost);
+						console.log(csvfirePoly[attr]);
+
+						topojsonProps[attr] = valCost;  // assign attribute and value to geojson properties
+
+						console.log(topojsonProps[attr]);
+					}));
+				};
+			};
+		};
+
+
+		console.log(csvfireData);
+		console.log(firePolygons);
+
+		return firePolygons;
+	};
+    //function to set the color classes and draw the states
 	function setEnumerationUnits(selectStates, map, path, colorScale){
 		var states = map.selectAll(".selectStates")
 			.data(selectStates)
@@ -233,10 +333,36 @@
 		var desc = states.append("desc")
 			.text('{"stroke": "#000", "stroke-width": "0.5px"}');
 	};
+    //define function to toggle fire polygons 
+	function toggleFires(path, map, firePolygons,colorScaleCost){
+		var fires = map.selectAll(".firePolygons")
+			.data(firePolygons)
+			.enter()
+			.append("path")
+			.attr("class", function (d) {
+				return "firePolygons " + d.properties.fire_name;
+			})
+			.attr("d", path)
+            .attr("opacity", 0)
+			.style("fill", function (d) {
+				return choroplethFire(d.properties, colorScaleCost);
+		})
+/*			.on("mouseover", function (d) {
+				highlight(d.properties);
+			})
+			.on("mouseout", function (d) {
+				dehighlight(d.properties);
+		})
+			.on("mousemove", moveLabel);
 
+		var desc = states.append("desc")
+			.text('{"stroke": "#000", "stroke-width": "0.5px"}');*/
+	};
 
+    
         //define function to toggle fire polygons 
-        function toggleFires(path, map, firePolygons) {
+/*
+        function toggleFires(path, map, firePolygons,colorScaleCost) {
            var fires = map.append("g");
             
            console.log(fires);
@@ -249,14 +375,17 @@
               .attr("stroke", "#999")
               .attr("d", path)
               .attr("class", "fire-polygons")
-              .attr("opacity", 0);
-            
+              .attr("opacity", 0)
+                .style("fill", function (d) {
+				    return choroplethFire(d.properties, colorScaleCost);
+            })
            console.log(fires);
         };
 
+*/
 
 
-	//function to test for data value and return color
+	//function to test for data value and return color for states
 	function choropleth(props, colorScale) {
 		//make sure attribute value is a number
 		var val = parseFloat(props[expressed]);
@@ -267,6 +396,19 @@
 			return "#CCC";
 		};
 	};
+    
+    //function to test for data value and return color for fire polygons
+	function choroplethFire(props2, colorScaleCost) {
+		//make sure attribute value is a number
+		var valCost = parseFloat(props2[costExpressed]);
+		//if attribute value exists, assign a color, otherwise assign gray
+		if (typeof valCost == 'number' && !isNaN(valCost)) {
+			return colorScaleCost(valCost);
+		} else {
+			return "#CCC";
+		};
+	};
+
 
 	// function to create a dropdown menu for attribute selection for states
 	function createDropdown(csvData) {
@@ -275,11 +417,11 @@
 			.append("select")
 			.attr("class", "dropdown")
 			.on("change", function(){
-				changeAttribute(this.value, csvData)
+				changeStateAttribute(this.value, csvData)
 			});
 
 		//add initial option
-		var titleOption = dropdown.append("option")
+		var titleOptionState = dropdown.append("option")
 			.attr("class", "titleOption")
 			.attr("disabled", "true")
 			.text("Select Year");
@@ -294,24 +436,23 @@
 
 	};
 	// function to create a dropdown menu for attribute selection for cost fire polys
-	function createCostFireDropdown(firePolygons) {
+	function createCostFireDropdown(csvfireData) {
 		//add select element
-		var costDropdown = d3.select(".cost-dropdown")
+		var dropdownCost = d3.select(".cost-dropdown")
 			.append("select")
 			.attr("class", "dropdown")
 			.on("change", function(){
-				// changeAttribute(this.value, firePolygons)
-				console.log("CHANGEDEDEDED");
+				changeFireAttribute(this.value, csvfireData)
 			});
 
 		//add initial option
-		var titleOption = costDropdown.append("option")
+		var titleOptionCost = dropdownCost.append("option")
 			.attr("class", "titleOption")
 			.attr("disabled", "true")
 			.text("Select Damage");
 
 		//add attribute name options
-		var attrOptionsCost = costDropdown.selectAll("attrOptionsCost")
+		var attrOptionsCost = dropdownCost.selectAll("attrOptionsCost")
 			.data(costAttrArray)
 			.enter()
 			.append("option")
@@ -320,10 +461,10 @@
 
 	};
 
-	//dropdown change listener handler
-	function changeAttribute(attribute, csvData){
+	//dropdown change listener handler for states
+	function changeStateAttribute(stateAttribute, csvData){
 		//change the expressed attribute
-		expressed = attribute;
+		expressed = stateAttribute;
 
 		console.log(expressed);
 		console.log(csvData);
@@ -356,19 +497,40 @@
 		updateChart(bars, csvData.length, colorScale);
 
 	};
+    
+    	//dropdown change listener handler for cost fire polygons
+	function changeFireAttribute(fireAttribute, csvfireData){
+		//change the expressed attribute
+		costExpressed = fireAttribute;
+
+		console.log(costExpressed);
+		console.log(csvfireData);
+
+		//recreate the color scale
+		var colorScaleCost = makeColorScaleCost(csvfireData);
+
+		//recolor enumeration units
+		var fires = d3.selectAll(".firePolygons")
+			.transition()
+			.duration(1000)
+			.style("fill", function(d){
+				return choroplethFire(d.properties, colorScaleCost)
+			});
+
+	};
 
 	function updateChart(bars, n, colorScale) {
 		// position bars
 		bars.attr("x", function(d, i){
-			return i * (chartInnerWidth / n) + leftPadding;
+			return i * (chartInnerWidth_S / n) + leftPadding_S;
 		})
 		//resize bars
 		.attr("height", function(d, i){
 			console.log(d[expressed]);
-			return 463 - yScale(parseFloat(d[expressed]));
+			return 463 - yScale_S(parseFloat(d[expressed]));
 		})
 		.attr("y", function(d, i){
-			return yScale(parseFloat(d[expressed])) + topBottomPadding;
+			return yScale_S(parseFloat(d[expressed])) + topBottomPadding_S;
 		})
 		//recolor bars
 		.style("fill", function(d){
@@ -385,16 +547,16 @@
 		//create a second svg element to hold the bar chart
 		var chart = d3.select("#chart-container")
 			.append("svg")
-			.attr("width", chartWidth)
-			.attr("height", chartHeight)
+			.attr("width", chartWidth_S)
+			.attr("height", chartHeight_S)
 			.attr("class", "chart");
 
 		//create a rectangle for chart background fill
 		var chartBackground = chart.append("rect")
 			.attr("class", "chartBackground")
-			.attr("width", chartInnerWidth)
-			.attr("height", chartInnerHeight)
-			.attr("transform", translate);
+			.attr("width", chartInnerWidth_S)
+			.attr("height", chartInnerHeight_S)
+			.attr("transform", translate_S);
 
 
 
@@ -409,7 +571,7 @@
 			.attr("class", function(d){
 				return "bar " + d.NAME;
 			})
-			.attr("width", chartInnerWidth / csvData.length -1)
+			.attr("width", chartInnerWidth_S / csvData.length -1)
 			.on("mouseover", highlight)
 			.on("mouseout", dehighlight)
 			.on("mousemove", moveLabel);
@@ -424,20 +586,22 @@
 
 		//create vertical axis generator
 		var yAxis = d3.axisLeft()
-			.scale(yScale);
+			.scale(yScale_S);
+        
+    
 
 		//place axis
 		var axis = chart.append("g")
 			.attr("class", "axis")
-			.attr("transform", translate)
+			.attr("transform", translate_S)
 			.call(yAxis);
 
 		//create frame for chart border
 		var chartFrame = chart.append("rect")
 			.attr("class", "chartFrame")
-			.attr("width", chartInnerWidth)
-			.attr("height", chartInnerHeight)
-			.attr("transform", translate);
+			.attr("width", chartInnerWidth_S)
+			.attr("height", chartInnerHeight_S)
+			.attr("transform", translate_S);
 
 		var desc = bars.append("desc")
 			.text('{"stroke": "none", "stroke-width": "0px"}');
